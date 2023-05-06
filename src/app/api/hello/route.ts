657fdbb1fcefb6d662bc35fe4082d8ed9c870664
ftapiat <francisco.tapia.t@gmail.com>
2023-05-06 16:55:12 -0400

    Add prettier

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,8 @@
+/**
+ * Returns a "Hello World" response from the API.
+ * @param {Request} request
+ * @constructor
+ */
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  return new Response("Hello, Next.js!");
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,56 @@
-'use client'
+"use client";
 
-import {FormEvent} from "react";
+import { FormEvent } from "react";
 
 type FormTarget = EventTarget & {
-    text: { value: string }
-}
+  text: { value: string };
+};
 
+/**
+ * Home page
+ * @constructor
+ */
 export default function Home() {
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        const target = event.target as FormTarget
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const target = event.target as FormTarget;
 
-        const data = {
-            first: target.text.value,
-        }
+    const data = {
+      first: target.text.value,
+    };
 
-        console.log({ data })
-        const JSONdata = JSON.stringify(data)
+    console.log({ data });
+    const JSONdata = JSON.stringify(data);
 
-        const endpoint = '/api/form'
+    const endpoint = "/api/form";
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSONdata,
-        }
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSONdata,
+    };
 
-        const response = await fetch(endpoint, options)
+    const response = await fetch(endpoint, options);
 
-        const result = await response.json()
-        alert(`Is this your full name: ${result.data}`)
-    }
+    const result = await response.json();
+    alert(`Is this your full name: ${result.data}`);
+  };
 
-    return (
-        <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <textarea name="text" id="text" cols={30} rows={10} className="bg-gray-300"></textarea>
-                <br/>
-                <button className="p-2 bg-gray-400">Enviar</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <textarea
+          name="text"
+          id="text"
+          cols={30}
+          rows={10}
+          className="bg-gray-100"
+        ></textarea>
+        <br />
+        <button className="p-2 bg-gray-400">Enviar</button>
+      </form>
+    </div>
+  );
 }
