2c22a7802625d865d1e1408c1179699e3a1e8f96
ftapia <ftapia@losheroes.cl>
2023-05-08 19:00:27 -0400

    Add a live test of HttpClient

--- src/app/api/compare-files/route.ts
+++ src/app/api/compare-files/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+import { HttpClient } from '@/models/http-client/HttpClient';
 
 /**
  * Returns a "Hello World" response from the API.
@@ -9,20 +10,39 @@ export async function POST(request: Request) {
   // Todo add validation
   const body = await request.json(); // TODO Cast to model
 
+  // Todo delete this test:
+  try {
+    const url = process.env.BACKEND_GET_FILE_VALUES_URL as string;
+    const payload = {
+      file: body.files[0],
+    };
+
+    const r = await HttpClient.setUrl(url)
+      .setMethodPost()
+      .setPayload(payload)
+      .call<never>();
+
+    console.log({ r });
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Todo end test, delete above code
+
+  const bodyJson = JSON.stringify(body);
   try {
     const options = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(body),
+      body: bodyJson,
     };
 
-    const res = await fetch(
-      process.env.BACKEND_COMPARE_FILES_URL as string,
-      options
-    ); // Todo move fetcher to model
-    console.log({ res });
+    const url = process.env.BACKEND_URL + '/compare/files'; // Todo replace
+    console.log(JSON.stringify({ url, bodyJson }));
+
+    const res = await fetch(url, options); // Todo move fetcher to model
     const resJson = await res.json(); // Todo cast to ApiModel
     return NextResponse.json(resJson.data); // Only return values
   } catch (error) {
